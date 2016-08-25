c36680d1f2b3dc268fec5f21222e623e6640846e
Dmitry Karpov <d.karpov@cherinfo.ru>
2016-08-25 14:31:27 +0300

    Поведение для спрайтов

--- assets/BaseAsset.php
+++ assets/BaseAsset.php
@@ -6,12 +6,14 @@ use yii\web\AssetBundle;
 
 class BaseAsset extends AssetBundle
 {
-	public static function getPublishedUrl() {
+	public static function getPublishedUrl() 
+	{
 		$obj = new static();
 		return \Yii::$app->assetManager->getPublishedUrl($obj->sourcePath).DIRECTORY_SEPARATOR;
 	}
 	
-	public static function getUrl($fileName){
+	public static function getUrl($fileName)
+	{
 		return static::getPublishedUrl().$fileName;
 	}
 }
---
+++ behaviors/SpriteModelBehavior.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace demetrio77\smartadmin\behaviors;
+
+use yii\base\Behavior;
+
+class SpriteModelBehavior extends Behavior
+{
+	public $spriteUrl;
+	public $spriteWidth;
+	public $spriteHeight;
+}
