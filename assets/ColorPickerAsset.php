f3f97dbf1d1d55658f7d8c0dc746b22ccf42fb1d
Dmitry Karpov <demetrio77@gmail.com>
2018-01-08 15:30:26 +0300

    Color picker updated

--- assets/ColorPickerAsset.php
+++ assets/ColorPickerAsset.php
@@ -6,7 +6,7 @@ use demetrio77\smartadmin\assets\BaseAsset;
 
 class ColorPickerAsset extends BaseAsset
 {
-	public $sourcePath = '@demetrio77/smartadmin/vendor/itsjavi/bootstrap-colorpicker/dist';
+	public $sourcePath = '@vendor/itsjavi/bootstrap-colorpicker/dist';
 	public $js  = ['js/bootstrap-colorpicker.min.js'];
 	public $css = ['css/bootstrap-colorpicker.min.css'];
 	public $depends = ['yii\web\JqueryAsset'];
