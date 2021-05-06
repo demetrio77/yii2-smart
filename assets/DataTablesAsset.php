193fa85c1d9127f69db7e41641080590c40ce636
Dmitry Karpov <demetrio77@gmail.com>
2021-05-06 15:22:12 +0300

    Datatables updated

--- assets/DataTablesAsset.php
+++ assets/DataTablesAsset.php
@@ -6,14 +6,10 @@ use demetrio77\smartadmin\assets\BaseAsset;
 
 class DataTablesAsset extends BaseAsset
 {
-	public $sourcePath = '@demetrio77/smartadmin/assets/files/js/plugin/';
+	public $sourcePath = '@demetrio77/smartadmin/assets/smartadmin/';
 
-	public $js  = ['datatables/jquery.dataTables.min.js', 
-		'datatables/dataTables.colVis.min.js',
-		'datatables/dataTables.tableTools.min.js',
-		'datatables/dataTables.bootstrap.min.js',
-		'datatable-responsive/datatables.responsive.min.js'
-	];
-	
+	public $js  = ['js/datagrid/datatables/datatables.bundle.js'];
+	public $css = ['css/datagrid/datatables/datatables.bundle.css'];
+
 	public $depends = ['demetrio77\smartadmin\assets\SmartAdminAsset', ];
 }
