381d1ff3248faf33de7dc07495fa6ca4e789bef3
Dmitry Karpov <demetrio77@gmail.com>
2022-12-26 20:59:39 +0300

    Store bootstrap-colorpicker in assets not as dependancy

--- assets/ColorPickerAsset.php
+++ assets/ColorPickerAsset.php
@@ -6,7 +6,7 @@ use demetrio77\smartadmin\assets\BaseAsset;
 
 class ColorPickerAsset extends BaseAsset
 {
-	public $sourcePath = '@vendor/itsjavi/bootstrap-colorpicker/dist';
+	public $sourcePath = '@demetrio77/smartadmin/assets/bootstrap-colorpicker-2.5.3/dist';
 	public $js  = ['js/bootstrap-colorpicker.min.js'];
 	public $css = ['css/bootstrap-colorpicker.min.css'];
 	public $depends = ['yii\web\JqueryAsset'];
