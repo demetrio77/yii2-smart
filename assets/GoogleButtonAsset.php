c4c202519b4ab2354de4f819098ea7914d0b1bea
Dmitry Karpov <demetrio77@gmail.com>
2024-05-09 20:16:09 +0300

    Google Sign-In button added to HTML helper

---
+++ assets/GoogleButtonAsset.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace demetrio77\smartadmin\assets;
+
+class GoogleButtonAsset extends BaseAsset
+{
+    public $sourcePath = '@demetrio77/smartadmin/assets/google-button';
+    public $css = ['google-button.css'];
+}
